313b6099ba394cb0615e7f71a707776ce88f4da0
Emily Costa <emilycosta@Emilys-MacBook-Pro-4.local>
2020-12-06 16:05:26 -0500

    styled navbar and created events page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from "./pages/contact/contact.component";
+import { EventsComponent } from './pages/events/events.component';
 import { HomeComponent } from "./pages/home/home.component";
 import { JoinUsComponent } from "./pages/join-us/join-us.component";
 import { LeadershipComponent } from "./pages/leadership/leadership.component";
@@ -9,6 +10,7 @@ import { LeadershipComponent } from "./pages/leadership/leadership.component";
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
+  { path: "events", component: EventsComponent },
   { path: "leadership", component: LeadershipComponent },
   { path: "joinus", component: JoinUsComponent },
   { path: "contact", component: ContactComponent },
