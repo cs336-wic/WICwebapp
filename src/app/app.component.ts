184feb6ce3f749ab128a028bb0c23a4bb5218766
sinai228 <purplelove228@gmail.com>
2020-12-08 16:00:46 +0900

    added button for darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'WICwebapp';
-}
+  title = "WICwebapp";
+
+  setDark = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+//   onChangeToggle() {
+//     this.setDark = !this.setDark;
+//     this.mode.emit(this.setDark);
+//     console.log(this.setDark);
+//   }
+// }
